9294dd8fbd862049572ba739e0528753ce3fcaad
Felix Heilmeyer <code@fehe.eu>
2021-08-18 10:38:16 +0200

    fix: test scripts

--- hack/test.sh
+++ hack/test.sh
@@ -1,7 +1,13 @@
 #!/bin/sh
-pushd hack
+set -e
+
+mkdir -p build
+cp hack/Dockerfile build/
+
+pushd build 
 docker build . -t keycloak-test
+popd
+
 docker run -d -p 9000:8080 -e KEYCLOAK_USER=admin -e KEYCLOAK_PASSWORD=admin \
   --name keycloak \
   keycloak-test
-popd
