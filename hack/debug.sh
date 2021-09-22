70830563ecac9f44cbeee3e62bdb68b1fb25eb44
Felix Heilmeyer <code@fehe.eu>
2021-09-22 13:03:47 +0200

    feat: add ukf-generic

--- hack/debug.sh
+++ hack/debug.sh
@@ -1,6 +1,12 @@
 #!/bin/sh
-docker run -d -p 9000:8080 -e KEYCLOAK_USER=admin -e KEYCLOAK_PASSWORD=admin \
+themes () {
+  for THEME in `ls theme`; do
+    echo "-v"
+    echo "$(pwd)/theme/${THEME}:/opt/jboss/keycloak/themes/${THEME}"
+  done
+}
+themes | xargs -J % docker run -d -p 9000:8080 -e KEYCLOAK_USER=admin -e KEYCLOAK_PASSWORD=admin \
   --name keycloak \
-  -v $(pwd)/theme/nail:/opt/jboss/keycloak/themes/nail \
+  % \
   -v $(pwd)/hack/no_cache.xml:/opt/jboss/keycloak/standalone/configuration/standalone-ha.xml \
   jboss/keycloak
